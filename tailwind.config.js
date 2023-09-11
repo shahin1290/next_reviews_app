6692d017913edc07ca43aacd6db4f0dfe2fad5db
shahin1290 <s.patowary@yahoo.com>
2023-09-11 13:00:39 +0200

    adds styling

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{jsx,tsx}", "./components/**/*.{jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
