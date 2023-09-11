6db65376983996602c05e0e0d7eb67fb6896b356
shahin1290 <s.patowary@yahoo.com>
2023-09-11 14:57:45 +0200

    assets and fonts

--- components/Heading.jsx
+++ components/Heading.jsx
@@ -1,3 +1,5 @@
+import { orbitron } from '@/app/fonts';
+
 export default function Heading({ children }) {
-  return <h1 className="font-bold pb-3 text-2xl">{children}</h1>;
+  return <h1 className={" font-orbitron font-bold pb-3 text-2xl"}>{children}</h1>;
 }
